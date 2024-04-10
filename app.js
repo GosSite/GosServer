86e3226531a80c4d539b94e8f985240c27e8a686
Witcher-MTM <phantomit228@gmail.com>
2024-04-10 18:30:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require("express")
 const app = express()
 const UserRouter = require('./Routes/UserRouter')
 const UserController = require('./Controller/User')
+const bodyParser = require('body-parser');
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 app.use('/user', UserRouter)
-app.use(express.json());
+app.use(bodyParser.json({ limit: '10mb' }));
 app.get('/',(req,res)=>{
     console.log('react native')
     UserController.updateUser(req,res)
