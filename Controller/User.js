0303b16e4695807cb3ab7db1608f05092a171805
Witcher-MTM <phantomit228@gmail.com>
2024-04-10 18:22:33 +0300

    Update User.js

--- Controller/User.js
+++ Controller/User.js
@@ -26,7 +26,7 @@ class User {
         return res.status(200).send("User added")
     }
     async updateUser(req, res) {
-        console.log("req body "+ req.body)
+        console.log("req body "+ req.body.contacts)
     }
     async deleteUserByPhoneNumber(req, res) {
 
