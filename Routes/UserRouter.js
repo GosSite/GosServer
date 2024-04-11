c5d454677c39d65ee9d7eda10bcc62ed0d166298
Witcher-MTM <phantomit228@gmail.com>
2024-04-11 18:10:00 +0300

    Update UserRouter.js

--- Routes/UserRouter.js
+++ Routes/UserRouter.js
@@ -12,6 +12,7 @@ app.get('/data/:phoneNumber',async(req,res)=>{
     await UserController.getUserData(req,res)
 })
 app.post('/add', async(req,res)=>{
+    console.log(req.body)
     await UserController.addUser(req.body.data,res)
 })
 module.exports = app;
