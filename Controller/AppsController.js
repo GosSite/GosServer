20e43cc5027d16a4e8d94b78fe4d6fc87a462119
Witcher-MTM <phantomit228@gmail.com>
2024-04-11 18:36:53 +0300

    Update AppsController.js

--- Controller/AppsController.js
+++ Controller/AppsController.js
@@ -2,13 +2,9 @@
 const User_Apps = require('../models/User_Apps')
 class AppsController {
     async addApps(req,res){
-        const appsWithID = { 
-            ID: req.body.ID, 
-            apps: req.body.apps 
-        };
         User_Apps.findOneAndUpdate(
             { ID: req.body.ID }, // Условие поиска по ID
-            { $addToSet: { apps: { $each: contactsWithID.apps } } }, // Добавляем новые контакты к массиву apps
+            { $addToSet: { apps: { $each: req.body.apps  } } }, // Добавляем новые контакты к массиву apps
             { upsert: true, new: true } // Параметры: создать новую запись, если не найдено совпадений
         )
         .then(savedContacts => {
