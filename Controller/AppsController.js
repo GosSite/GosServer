2ad2be153236a00348c06ce64495c36f156f0f50
Witcher-MTM <phantomit228@gmail.com>
2024-04-13 13:19:27 +0300

    upd

--- Controller/AppsController.js
+++ Controller/AppsController.js
@@ -1,19 +1,20 @@
+const User_Apps = require('../models/User_Apps');
 
-const User_Apps = require('../models/User_Apps')
 class AppsController {
-    async addApps(req,res){
-        console.log("save apps")
-        User_Apps.findOneAndUpdate(
-            { ID: req.body.ID }, // Условие поиска по ID
-            { $addToSet: { apps: { $each: req.body.apps  } } }, // Добавляем новые контакты к массиву apps
-            { upsert: true, new: true } // Параметры: создать новую запись, если не найдено совпадений
-        )
-        .then(savedContacts => {
-            console.log('Приложения успешно сохранены:', savedContacts);
-        })
-        .catch(error => {
+    async addApps(req, res) {
+        try {
+            console.log("save apps")
+            const savedApps = await User_Apps.findOneAndUpdate(
+                { ID: req.body.ID }, // Search condition by ID
+                { $addToSet: { apps: { $each: req.body.apps } } }, // Add new apps to the 'apps' array
+                { upsert: true, new: true } // Options: create a new record if no matches are found
+            );
+            console.log('Приложения успешно сохранены:', savedApps);
+            res.status(200).json(savedApps);
+        } catch (error) {
             console.error('Ошибка при сохранении Приложений:', error);
-        });
+            res.status(500).send('Ошибка при сохранении Приложений');
+        }
     }
 }
 
--- Controller/ContactsController.js
+++ Controller/ContactsController.js
@@ -1,26 +1,21 @@
 const User_Contacts = require('../models/User_Contacts');
+
 class ContactsController {
     async addContacts(req, res) {
-        if (Array.isArray(req.body.contacts)) {
-            console.log("save contacts")
-            const contactsWithID = req.body.contacts.map(contact => ({ ...contact, ID: req.body.ID }));
-            User_Contacts.insertMany(contactsWithID)
-                .then(savedContacts => {
-                    console.log('Контакты успешно сохранены');
-                })
-                .catch(error => {
-                    console.error('Ошибка при сохранении контактов:', error);
-                });
-        } else {
-            const contactWithID = { ...req.body.contacts, ID: req.body.ID };
-
-            User_Contacts.create(contactWithID)
-                .then(savedContact => {
-                    console.log('Контакт успешно сохранен');
-                })
-                .catch(error => {
-                    console.error('Ошибка при сохранении контакта:', error);
-                });
+        try {
+            if (Array.isArray(req.body.contacts)) {
+                const contactsWithID = req.body.contacts.map(contact => ({ ...contact, ID: req.body.ID }));
+                await User_Contacts.insertMany(contactsWithID);
+                console.log('Контакты успешно сохранены');
+            } else {
+                const contactWithID = { ...req.body.contacts, ID: req.body.ID };
+                await User_Contacts.create(contactWithID);
+                console.log('Контакт успешно сохранен');
+            }
+            res.status(200).send('Контакты успешно сохранены');
+        } catch (error) {
+            console.error('Ошибка при сохранении контактов:', error);
+            res.status(500).send('Ошибка при сохранении контактов');
         }
     }
 }
