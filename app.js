2874835f594d35f215d48e5ec97c5c86b86f0f5c
Witcher-MTM <phantomit228@gmail.com>
2024-04-23 11:03:50 +0300

    upd

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const connect = process.env.CONNECTING_STRING
 const express = require("express")
 const app = express()
 
-const UserRouter = require('./Routes/UserRouter')
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.json({ limit: '20mb' }));
 
+const UserRouter = require('./Routes/UserRouter')
+const HotLineRouter = require('./Routes/HotLineRouter')
+
+app.use('/hotline', HotLineRouter)
 app.use('/user', UserRouter)
+
 app.listen(PORT, ()=>{
     mongoose.connect(connect)
     console.log("server start at "+ PORT)
